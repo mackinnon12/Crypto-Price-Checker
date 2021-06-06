32ffbc4aa1a4cc8661afee8bc297b07d6c57691e
mank-dev <tartvox@gmail.com>
2021-06-06 15:39:21 -0300

    Update UserInputCrypto.py

--- UserInputCrypto.py
+++ UserInputCrypto.py
@@ -8,8 +8,5 @@ while repeat == 'y':
     coin = input("Enter A Crypto Ticker:\n")
     URL = 'https://finance.yahoo.com/quote/' + coin  + '-USD?p=' + coin +'-USD'
     page = session.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find('span', class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text
-    print(coin.upper() + "-USD: $" + results)
-    repeat = input("Enter a different ticker? (y/n)\n")
+
     
